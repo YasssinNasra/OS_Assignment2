762352854bab37ef68e88ac6076a35c36a673a7f
Yassin Nasra <yassin.kamaleldeen.2024@aiu.edu.eg>
2025-11-20 22:15:44 +0200

    file changes

--- loaderProgram/program.c
+++ loaderProgram/program.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
+//the package needed for the program which is used by the main function
 int main(){
+// main function which contains a simple printf statement
 printf("This program is a simple program created by yassin nasra\n");
 return 0;
 }
