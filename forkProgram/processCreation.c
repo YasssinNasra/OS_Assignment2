b9c2f1dc1d218a3b7114c54390454436854361c1
Yassin Nasra <yassin.kamaleldeen.2024@aiu.edu.eg>
2025-11-20 21:33:44 +0200

    changed processCreationComments

--- forkProgram/processCreation.c
+++ forkProgram/processCreation.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
+// The previous 2 lines show the imports needed for the program 
 int main(){
+// The main is what is ran or looked for by the compiler to be able to run the program 
 pid_t pid = fork();
+// fork() is system call used to create new process 3 outputs bigger than 0 parent, equal to 0 child and negative fail 
+
 if (pid == 0){
 printf("This process is the child process PID: %d\n",getpid());//This block child process as the pid is equal to 0
 }
@@ -9,9 +13,9 @@ else if (pid > 0){
 printf("This process is the parent process PID: %d\n",getpid());//This block parent process as the PID is greater than 0
 }
 else{
-printf("Fork Failed!");// the fork will fail if the pid is less than 0  and this happen when no resources for process
+printf("Fork Failed!");// the fork will fail if the pid is less than 0 = negative number
 }
 return 0;
-//This is where the program process end
+//This is where the program process ends
 
 }
